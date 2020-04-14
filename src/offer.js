844f0e4a8bdcfeb0927c8800e848a91aa438ee12
José Gilberto Mullor <gilber@gmail.com>
2020-04-14 16:54:56 +0200

    - Make some improvements to API - Add GET offer endpoint

--- src/offer.js
+++ src/offer.js
@@ -1,24 +1,52 @@
 'use strict';
 const Response = require('../lib/response');
 const Salesforce = require("../lib/salesforce");
+const SalesforceFields = require("../lib/salesforceFields");
+const sendExceptionEmail = require("../lib/exception");
 
-module.exports.get = (event, context, callback) => {
-    const salesforce = new Salesforce(
+module.exports.get = async (event, context, callback) => {
+    const salesforceService = new Salesforce(
         process.env.RA_SALESFORCE_USERNAME,
         process.env.RA_SALESFORCE_PASSWORD
     );
 
-    if (!("reference" in event.pathParameters)) {
-        callback(null, Response.createPlainTextResponse('Reference not found in parameters', 404));
+    if(!("pathParameters" in event) || !(event.pathParameters instanceof Object)) {
+        return callback(null, Response.createPlainTextResponse('Missing parameters in request', 404));
     }
-    const reference = event.pathParameters.reference;
 
-    salesforce.getOfferByReference(reference)
-        .then(offer => {
-            callback(null, Response.createJsonResponse(offer));
-        })
-        .catch(err => {
-            console.log('getOffer Error', err);
-            callback(null, Response.createPlainTextResponse(`Offer with reference '${reference}' does not exist`, 404));
-        });
+    if (!("reference" in event.pathParameters)) {
+        return callback(null, Response.createPlainTextResponse('Reference not found in parameters', 404));
+    }
+
+    if (!("id" in event.pathParameters)) {
+        return callback(null, Response.createPlainTextResponse('Id not found in parameters', 404));
+    }
+
+    const reference = event.pathParameters.reference;
+    const id = event.pathParameters.id;
+
+    try {
+        let offer = await salesforceService.getObject(reference);
+
+        if (offer[SalesforceFields.FIELD_ID].toLowerCase() !== id.toLowerCase()) {
+            return callback(null, Response.createPlainTextResponse(`Invalid id for offer with reference '${reference}'`, 404));
+        }
+
+        let returnedOffer = {
+            'id':           offer[SalesforceFields.FIELD_ID].toLowerCase(),
+            'reference':    offer[SalesforceFields.FIELD_REFERENCE],
+            'createdAt':    offer[SalesforceFields.FIELD_SALESFORCE_CREATED_DATE],
+            'quantity':     offer[SalesforceFields.FIELD_QUANTITY],
+            'type':         offer[SalesforceFields.FIELD_TYPE],
+            'details':      offer[SalesforceFields.FIELD_DETAILS],
+            'other':        offer[SalesforceFields.FIELD_OTHER],
+        }
+
+        return callback(null, Response.createJsonResponse(returnedOffer));
+    } catch (ex) {
+        console.log('getOffer Error', ex);
+        sendExceptionEmail(ex, event, context);
+        
+        return callback(null, Response.createPlainTextResponse(`Offer with reference '${reference}' does not exist`, 404));
+    }
 };
--- src/request.js
+++ src/request.js
@@ -4,10 +4,7 @@ const Recaptcha = require("../lib/recaptcha");
 const RequestFormSchema = require("./schema/request");
 const AskFormSchema = require("./schema/ask");
 const ValidateOrFail = require("../lib/validate");
-
-const Ses = require("../lib/ses");
-const Mailer = require('../lib/mailer');
-const mailerService = new Mailer(Ses);
+const sendExceptionEmail = require("../lib/exception");
 
 const Salesforce = require('../lib/salesforce');
 const salesforceService = new Salesforce(
@@ -15,26 +12,12 @@ const salesforceService = new Salesforce(
     process.env.RA_SALESFORCE_PASSWORD
 );
 
-function sendExceptionEmail(ex, event, context) {
-    let htmlBody = `<html><body><h3>${ex}</h3><p><pre>${JSON.stringify(ex.stack, null, 4)}</pre></p><p><strong>event:</strong><pre>${JSON.stringify(event, null, 4)}</pre></p><p><strong>context:</strong><pre>${JSON.stringify(context, null, 4)}</pre></p></body></html>`;
-    let plainBody = `${ex}\n${JSON.stringify(ex.stack, null, 4)}\n\n\nevent:\n${JSON.stringify(event, null, 4)}\n\n\ncontext:\n${JSON.stringify(context, null, 4)}`;
-
-    mailerService.send({
-        to: [process.env.RA_EXCEPTION_EMAIL_NOTIFICATIONS],
-        fromName: process.env.RA_FROM_NAME,
-        fromEmail: process.env.RA_FROM_EMAIL,
-        subject: `[${Date.now()}] rescueapp.es-api Error: ${ex}`,
-        htmlBody: htmlBody,
-        plainBody: plainBody,
-    })
-}
-
 function isTest(event) {
     if (
         null !== event.queryStringParameters
         && 'queryStringParameters' in event
         && 'test' in event.queryStringParameters
-        && Boolean(event.queryStringParameters.test)
+        && '1' === event.queryStringParameters.test
     ) {
         return true;
     }
